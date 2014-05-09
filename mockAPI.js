eac71455702ba7c940511a984180fb402a1d07b1
Isaac Chansky <ichansky@gmail.com>
2014-05-09 11:59:08 -0400

    remove prompt stuff

--- mockAPI.js
+++ mockAPI.js
@@ -1,23 +1,8 @@
 #!/usr/bin/env node
 
-var prompt = require('prompt');
-var chalk = require('chalk');
-
 var express = require('express');
 
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
-
-
-function parseVerbs(err, result){
-  if(err){ return onErr(err); }
-  var verbs = result.verbs
-    .split(' ')
-    .map( function(item){ return item.trim().toUpperCase(); } );
-  console.log('\n\ncool, we will use: '+verbs+'\n'); 
   
-}
-
 function onErr(err){
   console.log(err);
   return 1;
@@ -34,13 +19,10 @@ function onErr(err){
 //  ]
 // }
 function makeAPI(options){
-}
+  var app = express();
+  resources.forEach(makeEndpoints);
 
-// set up prompt
-prompt.message = ">>".green;
-console.log(chalk.blue("\nLet's make you an api (just one resource for now)!!\n"));
-console.log(chalk.magenta('What REST verbs do you want to use for this resource? ( get post put delete)\n Separate by spaces.'));
-prompt.start();
-prompt.get(['verbs'], parseVerbs);  
+  return app;
+}
 
 
